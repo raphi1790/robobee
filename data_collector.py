6f904464498e6a4cd528782937c3a80520bbf08a
Raphael Schönenberger <raphael.schoenenberger@gmail.com>
2021-03-01 22:59:55 +0100

    finish data_collector

--- data_collector.py
+++ data_collector.py
@@ -1,56 +1,85 @@
 import json
 from websocket import create_connection
 from influxdb import InfluxDBClient
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytz
 
 
+def start_websocket_connection():
+    uri = "wss://ws.bitstamp.net"
+    ws = create_connection(uri)
+    return ws
 
-uri = "wss://ws.bitstamp.net"
-ws = create_connection(uri)
+def collect_websocket_data(websocket, aggregation_level=30):
+    # open connection
+    client = InfluxDBClient('localhost', 8086, 'root', 'Gotthard', 'pi_influxdb')
 
-data = {
+    # request websocket data
+    websocket_request_data =  {
         "event": "bts:subscribe",
         "data": {
             "channel": "live_trades_btcusd"
         }
     }
-data_json = json.dumps(data)
+    websocket_request_data_json = json.dumps(websocket_request_data)
+    websocket.send(websocket_request_data_json) # start requesting websocket-data
 
 
+    while True:
+        start_time = datetime.now(tz=pytz.utc)
+        current_time = start_time
+        interval = aggregation_level # aggregate all transactions within the time-interval into one point
+        values = []
+        while current_time < start_time + timedelta(seconds=interval):
+            try:
+                result = websocket.recv()
+                obj = json.loads(result)
 
+                if bool(obj['data']) :
+                    price = obj['data']['price']
+                    timestamp = obj['data']['timestamp']
+                    localized_timestamp = datetime.fromtimestamp(int(timestamp)).astimezone(pytz.utc)
+                    values.append(price)
 
-client = InfluxDBClient('localhost', 8086, 'root', 'Gotthard', 'pi_influxdb')
-print("client", client)
-
-dbs = client.get_list_database()
-print("dbs", dbs)
-
-ws.send(data_json)
-while True:
-    try:
-        result = ws.recv()
-        obj = json.loads(result)
-        if bool(obj['data']) :
-            price = obj['data']['price']
-            timestamp = obj['data']['timestamp']
-            print("timestamp", datetime.fromtimestamp(int(timestamp)))
-            print("obj", obj)
-            json_body = [
-            {
-                "measurement": "bitcoin_price",
-                "tags": {
-                    "currency": "USD",
-                    "exchange": "Bitstamp"
-                   
-                },
-                "time": datetime.fromtimestamp(int(timestamp)),
-                "fields": {
-                    "value": price
-                }
-            }
+                    
+                current_time = datetime.now(tz=pytz.utc)
+            except Exception as e:
+                print(e)
+                break
+        
+        average_value = sum(values)/len(values) if len(values) > 0 else 0
+        print("values", values)
+        print("average_value",average_value )
+        print("len(values)", len(values))
+        if len(values) > 0:
+            single_point = [
+                    {
+                        "measurement": "bitcoin_price",
+                        "tags": {
+                            "currency": "USD",
+                            "exchange": "Bitstamp"
+                        
+                        },
+                        "time": current_time,
+                        "fields": {
+                            "value": average_value
+                        }
+                    }
             ]
-            # print("json_body",json_body)
-            # client.write_points(json_body)
-    except Exception as e:
-        print(e)
-        break
+            client.write_points(single_point )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from influxdb import InfluxDBClient
+from data_collector import start_websocket_connection, collect_websocket_data
 
-client = InfluxDBClient('localhost', 8086, 'root', 'Gotthard', "pi_influxdb")
-print("client", client)
+if __name__ == "__main__":
+    ws = start_websocket_connection()
+    collect_websocket_data(ws, aggregation_level=30)
 
-dbs = client.get_list_database()
-print("dbs", dbs)
 
-results = client.query('SELECT * FROM "pi_influxdb"."autogen"."bitcoin_price" order by time desc limit 10')
+# client = InfluxDBClient('localhost', 8086, 'root', 'Gotthard', "pi_influxdb")
+# print("client", client)
 
-print("results", results)
+# dbs = client.get_list_database()
+# print("dbs", dbs)
+
+# results = client.query('SHOW RETENTION POLICIES')
+
+# print("results", results)
