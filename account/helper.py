ffa3f5d7caccb15971379ceb745eeb399f5a87e5
Raphael Schönenberger <raphael.schoenenberger@gmail.com>
2021-04-04 13:59:05 +0200

    add fallback

--- account/helper.py
+++ account/helper.py
@@ -66,13 +66,16 @@ def _upper_threshold_is_satisfied(current_stock_price,past_stock_prices_1d,past_
     is_above_upper_threshold_7d = _is_above_threshold(current_stock_price,past_stock_prices_7d_1d,buying_margin)
     is_alltime_high = is_above_upper_threshold_1d  and not is_above_upper_threshold_7d
     if(is_alltime_high):
-        # Increase buying_margin in order to deal with recen alltime-highs
+        # Increase buying_margin in order to deal with recent alltime-highs
         return _is_above_threshold(current_stock_price,past_stock_prices_1d,1.5*buying_margin)
     else:
         return _is_above_threshold(current_stock_price,past_stock_prices_1d,buying_margin)
-    
 
-def _is_buyable(reserve, buying_margin, available_eur,available_eth, current_stock_price, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m, last_selling_datetime):
+def _is_below_threshold(relevant_stock_price,past_stock_prices,margin):
+    values_below_threshold = [value for value in past_stock_prices if value < relevant_stock_price / (1+margin)]
+    return len(values_below_threshold)>3   
+
+def _is_buyable(reserve, buying_margin, available_eur,available_eth, current_stock_price, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m, last_selling_datetime, trend):
     tradeable_budet=available_eur-reserve
     buying_power_condition=tradeable_budet>(available_eth*current_stock_price)
     upper_threshold_condition_shorterm = _is_above_threshold(current_stock_price,past_stock_prices_10m,buying_margin)
@@ -81,8 +84,8 @@ def _is_buyable(reserve, buying_margin, available_eur,available_eth, current_sto
         # no_trade_condition = True
     # else:
         # no_trade_condition = datetime.now() - timedelta(days=1) > last_selling_datetime 
-    trend=_determine_trend(past_stock_prices_10m)
-    print("trend", trend)
+    
+    
     if ((buying_power_condition and upper_threshold_is_satisfied_longterm and not (trend == 'decreasing') ) or
         (buying_power_condition and trend == 'increasing' and upper_threshold_condition_shorterm) ):
         if (buying_power_condition and upper_threshold_is_satisfied_longterm and not (trend == 'decreasing')):
@@ -93,29 +96,49 @@ def _is_buyable(reserve, buying_margin, available_eur,available_eth, current_sto
     
     return False
 
+def _buy_fallback(last_selling_datetime, last_buying_datetime, last_buying_price, available_eth, available_eur, reserve,past_stock_prices_1d,past_stock_prices_10m, current_stock_price, trend ):
+    buying_was_last_trade = last_selling_datetime < last_buying_datetime # buying was the last trade
+    eth_are_already_bought = available_eth > 0.03 
+    only_eur_reserve_is_available = available_eur < reserve * 1.01
+    lower_trend_than_last_buying = _is_below_threshold(last_buying_price,past_stock_prices_1d,0.2 )
+    current_price_condition = current_stock_price * 1.2< last_buying_price 
+    if(buying_was_last_trade and eth_are_already_bought and only_eur_reserve_is_available and
+       lower_trend_than_last_buying and current_price_condition and not (trend == 'decreasing') ):
+       return True
+    return False
 
-def _is_sellable(selling_margin, available_eth, current_stock_price, last_buying_price, past_stock_prices_10m):
+def _is_sellable(selling_margin, available_eth, current_stock_price, last_buying_price, past_stock_prices_10m,trend, available_eur, reserve):
     modified_buying_price=last_buying_price or 10000 # 10000 is just a default value in case of None
     stock_price_condition= (1+selling_margin)*modified_buying_price < current_stock_price
     available_eth_condition=available_eth>=0.03
-    trend=_determine_trend(past_stock_prices_10m)
-    if (available_eth_condition and stock_price_condition and not (trend == 'increasing')):
+    fallback_condition_eur = available_eur < 0.1 * reserve
+    fallback_condition_price = (1.12)*modified_buying_price < current_stock_price # calculated amount, where we still make profit; TBD write function for that
+    if ((available_eth_condition and stock_price_condition and not (trend == 'increasing')) or
+        (fallback_condition_eur and fallback_condition_price )) :
         return True
     return False
 
-def determine_status(available_eur, available_eth, current_stock_price, RESERVE, FEE, BUYING_MARGIN, SELLING_MARGIN, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m,last_selling_datetime, last_buying_price):
+
+
+def determine_status(available_eur, available_eth, current_stock_price, RESERVE, FEE, BUYING_MARGIN, SELLING_MARGIN, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m,last_selling_datetime, last_selling_price, last_buying_datetime, last_buying_price):
     is_plausible = _is_plausible(available_eur, available_eth, current_stock_price, RESERVE, FEE, BUYING_MARGIN, SELLING_MARGIN, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m)
     print("is_plausible",is_plausible)
 
     if is_plausible:
-        is_buyable = _is_buyable(RESERVE, BUYING_MARGIN, available_eur,available_eth, current_stock_price, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m, last_selling_datetime)
-        is_sellable = _is_sellable(SELLING_MARGIN, available_eth, current_stock_price, last_buying_price, past_stock_prices_10m)
+        trend=_determine_trend(past_stock_prices_10m)
+        print("trend", trend)
+        is_buyable = _is_buyable(RESERVE, BUYING_MARGIN, available_eur,available_eth, current_stock_price, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m, last_selling_datetime, trend)
+        is_sellable = _is_sellable(SELLING_MARGIN, available_eth, current_stock_price, last_buying_price, past_stock_prices_10m, trend, available_eur, RESERVE)
+        buy_fallback = _buy_fallback(last_selling_datetime, last_buying_datetime, last_buying_price, available_eth, available_eur, RESERVE,past_stock_prices_1d,past_stock_prices_10m, current_stock_price, trend)
         print("is_buyable", is_buyable)
         print("is_sellable", is_sellable)
+        print("buy_fallback", buy_fallback )
         if is_buyable:
             return 'buy'
         if is_sellable:
             return 'sell'
+        if buy_fallback:
+            return 'buy_fallback'
 
     return None
 
--- account/simple_robobee.py
+++ account/simple_robobee.py
@@ -19,9 +19,9 @@ def collect_honey():
         past_stock_prices_1d = get_eth_eur_values(from_dt_str='now() - 1d' , to_dt_str='now()')
         past_stock_prices_7d_1d = get_eth_eur_values(from_dt_str='now() - 7d' , to_dt_str='now() - 1d')
         past_stock_prices_10m = get_eth_eur_values(from_dt_str='now() - 10m' , to_dt_str='now()')
-        _, last_buying_price = get_last_transaction_price('buy')
-        last_selling_datetime, _ = get_last_transaction_price('sell')
-        action = determine_status(available_eur, available_eth, current_stock_price, RESERVE, FEE, BUYING_MARGIN, SELLING_MARGIN, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m,last_selling_datetime, last_buying_price)
+        last_buying_datetime, last_buying_price = get_last_transaction_price('buy')
+        last_selling_datetime, last_selling_price = get_last_transaction_price('sell')
+        action = determine_status(available_eur, available_eth, current_stock_price, RESERVE, FEE, BUYING_MARGIN, SELLING_MARGIN, past_stock_prices_1d,past_stock_prices_7d_1d, past_stock_prices_10m,last_selling_datetime, last_selling_price,last_buying_datetime, last_buying_price)
         print("action", action)
         if action == 'buy':
             tradeable_budget=available_eur - RESERVE
@@ -29,10 +29,17 @@ def collect_honey():
             print("tradeable_budget", tradeable_budget)
             print("buying_eth", buying_eth)
             buy_eth(buying_eth)
-        
+        if action == 'buy_fallback':
+            tradeable_budget=RESERVE
+            buying_eth = calculate_eth(tradeable_budget, current_stock_price)
+            print("tradeable_budget", tradeable_budget)
+            print("buying_eth", buying_eth)
+            buy_eth(buying_eth)
+
         if action == 'sell':
             print("selling available_eth", available_eth)
             sell_eth(available_eth)
+
         
         time.sleep(30)
         
